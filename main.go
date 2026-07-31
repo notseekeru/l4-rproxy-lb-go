4b17f6c6875d7eec516cbd9f0035776407e831f8
Stephen Macabulos <myteseeker18@gmail.com>
2026-07-31 11:56:27 +0800

    feat(main): implement TCP server and connection handling

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	serverConnection, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Printf("Error connecting to server: %v", err)
+		return
+	}
+	defer serverConnection.Close()
+
 }
